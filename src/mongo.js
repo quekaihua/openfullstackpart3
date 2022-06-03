dec2a69286c0e0047548039fdb6e74db73a7233d
qkh <myshengun@163.com>
2022-06-04 06:10:55 +0800

    fix code style

--- src/mongo.js
+++ src/mongo.js
@@ -1,52 +1,52 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
   console.log(
-    "Please provide the password as an argument: node mongo.js <password>"
-  );
-  process.exit(1);
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
 }
 
 const getId = () => {
-  return Math.floor(Math.random() * Math.pow(2, 30));
-};
+  return Math.floor(Math.random() * Math.pow(2, 30))
+}
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://root:${password}@cluster0.puvpf.mongodb.net/phonebook?retryWrites=true&w=majority`;
+const url = `mongodb+srv://root:${password}@cluster0.puvpf.mongodb.net/phonebook?retryWrites=true&w=majority`
 
-mongoose.connect(url);
+mongoose.connect(url)
 
 const personScheme = new mongoose.Schema({
   name: String,
   number: String,
   id: Number,
-});
+})
 
-const Person = mongoose.model("Person", personScheme);
+const Person = mongoose.model('Person', personScheme)
 
 if (process.argv.length === 3) {
   Person.find({}).then((res) => {
-    console.log("phonebook:");
+    console.log('phonebook:')
     res.forEach((person) => {
-      console.log(person.name, " ", person.number);
-    });
+      console.log(person.name, ' ', person.number)
+    })
 
-    mongoose.connection.close();
-  });
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-  const name = process.argv[3];
-  const number = process.argv[4];
-  const id = getId();
+  const name = process.argv[3]
+  const number = process.argv[4]
+  const id = getId()
 
   const person = new Person({
     name,
     number,
     id,
-  });
+  })
 
-  person.save().then((res) => {
-    console.log(`add ${name} number ${number} to phonebook`);
-    mongoose.connection.close();
-  });
+  person.save().then(() => {
+    console.log(`add ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
