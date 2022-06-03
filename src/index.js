df7ce87046b47a13336878ab35f26217ea410b5f
qkh <myshengun@163.com>
2022-06-03 22:42:29 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(cors());
+
+var morgan = require("morgan");
+morgan.token("postformat", function (req) {
+  return JSON.stringify(req.body);
+});
+
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postformat"
+  )
+);
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const getId = () => {
+  return Math.ceil(Math.random() * Math.pow(2, 10));
+};
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  res.status(204).end();
+});
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: "name missing",
+    });
+  } else if (!body.number) {
+    return res.status(400).json({
+      error: "number missing",
+    });
+  }
+
+  const exists = persons.find((person) => person.name === body.name);
+  if (exists) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: getId(),
+  };
+
+  persons = persons.concat(person);
+  res.json(person);
+});
+
+app.get("/info", (req, res) => {
+  console.log(req.headers);
+  result = `<div>Phonebook has info for ${persons.length} people</div>`;
+  const now = new Date();
+  result += `<div>${now.toLocaleString()}  Beijing Standard Time</div`;
+  return res.send(result);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
